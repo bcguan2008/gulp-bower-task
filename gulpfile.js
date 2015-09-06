ea5e873a71753513964c499ba50783cd27405756
ben <bcguan2008@gmail.com>
2015-09-06 15:25:53 +0800

    no message

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var gbowerTask = require('gulp-bower-task');
-
-gulp.task('default',function(){
-	gulp.src('./bower.json')
-		.pipe(gbowerTask())
-		.pipe(gulp.dest('./Tests/output/'));
-})
