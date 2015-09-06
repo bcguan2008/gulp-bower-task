138b1bb8b76aceca63295624c55dccfc73207661
ben <bcguan2008@gmail.com>
2015-09-06 15:23:22 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
-var gbowerTask = require('./index.js');
+var gbowerTask = require('gulp-bower-task');
 
 gulp.task('default',function(){
 	gulp.src('./bower.json')
 		.pipe(gbowerTask())
-		.pipe(gulp.dest('./out/'));
+		.pipe(gulp.dest('./Tests/output/'));
 })
